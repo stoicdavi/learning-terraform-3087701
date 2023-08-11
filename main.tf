c004f0065f3ca0c126da5c93c53e0a1337e16c4b
Josh Samuelson <josh@joshsamuelson.com>
2023-08-11 12:53:30 -0700

    Revert "Create new vpc"
    This reverts commit b967df868738dfc52afa67ad99fab97c55fd34f5.

--- main.tf
+++ main.tf
@@ -18,28 +18,10 @@ data "aws_vpc" "default" {
   default = true
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
 
 
   tags = {
@@ -51,7 +33,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.vpc_id
+  vpc_id  = data.aws_vpc.default.id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
