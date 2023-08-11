7bd695398702cfdd08756ade4d0f47253249a6e0
Josh Samuelson <josh@joshsamuelson.com>
2023-08-11 12:36:11 -0700

    fix typo

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.vpc_id
+  vpc_id  = data.aws_vpc.default.id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
